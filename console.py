88e87236183b7651dfe81e867f8bc9628c181d08
abrahamof2016 <jarageda9@gmail.com>
2024-03-24 17:45:34 +0000

    update console.

--- console.py
+++ console.py
@@ -7,12 +7,12 @@ import cmd
 import shlex
 import models
 from models.base_model import BaseModel
-# from models.user import User
-# from models.place import Place
-# from models.state import State
-# from models.city import City
-# from modlels.amenity import Amenity
-# from models.review import Review
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
